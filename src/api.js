6804fb2a741a1367a9d05fdb71c69290b020272c
Ryan Glazer <rglazer003@gmail.com>
2018-12-21 09:22:40 -0600

    POST is working

--- src/api.js
+++ src/api.js
@@ -1,11 +1,12 @@
 module.exports = {
   getMovies: () => {
-    return fetch('http://localhost:1313/api/movies')
+    return fetch('/api/movies')
       .then(response => response.json());
   },
   addMovies: (newMovie) => {
-      return fetch('http://localhost:1313/api/movies', {
-        method: 'POST', header: {'Content-Type': 'application/json'}, body: JSON.stringify(newMovie)
+      console.log(newMovie);
+      return fetch('/api/movies', {
+        method: 'POST', headers: {'Content-Type': 'application/json'}, body: JSON.stringify(newMovie)
       })
 
   }
